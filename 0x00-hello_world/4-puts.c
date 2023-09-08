b3eebd563fc8ce169f4016e4f2c07e880fce8916
WolaDev <bozzbhoi@gmail.com>
2023-09-08 00:09:34 -0700

    My first C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
  * WolaDev
  * A student at Alx_Africa
  * Full Stack Software Engineering Student
- *A program to display:
+*/
+#include <stdio.h>
+
+/*A program to display:
  * "Programming is like building a multilingual puzzle"
  * with puts not printf
 */
-
-#include <stdio.h>
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
