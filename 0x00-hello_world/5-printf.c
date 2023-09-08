67b2930d8da0d6cfa231b2cf176836b01e1ab6a8
WolaDev <bozzbhoi@gmail.com>
2023-09-08 09:41:39 -0700

    Second C program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,9 @@
  * main - "with proper grammar, but the outcome is a piece of art,"
  * Return: Always 0 (Success).
  */
-int main(void) {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+int main(void)
+{
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
 
 	return(0);
 }
