fd7fd44698c37e9bb37eaa73949543d6771bcdd4
WolaDev <bozzbhoi@gmail.com>
2023-09-08 01:05:19 -0700

    My first C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 
 /**
  * main - "Programming is like building a multilingual puzzle"
- * Retun: Always 0 (Success).
+ * Return: Always 0 (Success).
  */
 int main(void)
 /*Initial*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	/*Return: Always 0*/
+	
 	return (0);
 }
