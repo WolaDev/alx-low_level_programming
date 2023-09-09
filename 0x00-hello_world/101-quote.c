4ce9672f8d6b92194e3d604dd991c6b5203dbe43
WolaDev <bozzbhoi@gmail.com>
2023-09-09 12:39:23 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 int main(void) {
-  // Write the quote to standard error
-  write(2, "and that piece of art is useful" "\n" "- Dora Korpar, 2015-10-19", 42);
-
-  // Return 1
-  return 1;
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return 1;
 }
