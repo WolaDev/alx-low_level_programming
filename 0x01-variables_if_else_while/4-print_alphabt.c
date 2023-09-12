e0067bdccdef55c192b3abf4cedfade239300454
WolaDev <bozzbhoi@gmail.com>
2023-09-12 15:08:06 -0700

    alphabet with no 'e' and 'q'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase in exception of 'q' and 'e'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+	letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+		putchar(letter);
+		}
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
+
