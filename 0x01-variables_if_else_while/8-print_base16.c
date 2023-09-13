dd17129f8242b01571ca8d2d2cc23ca99d464c65
WolaDev <bozzbhoi@gmail.com>
2023-09-13 04:44:53 -0700

    Print base 16 in hexadecimal lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char base;
+
+	for (base = '0'; base <= 9; base++)
+	{
+		putchar(base);
+	}
+
+	for (base = 'a'; base <= 'f'; base++)
+	{
+		putchar(base);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
