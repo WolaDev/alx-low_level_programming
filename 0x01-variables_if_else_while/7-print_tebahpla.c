b9a6ba0dbfff847c01de9bbb63363acb4e8acb93
WolaDev <bozzbhoi@gmail.com>
2023-09-13 00:39:36 -0700

    Alphabet in descending order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet in descending order
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char d;
+	d = 'z';
+
+	while (d >= 'a')
+	{
+		putchar(d);
+		d--;
+	}
+	putchar('\n');
+
+	return (0);
+}
