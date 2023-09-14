281505d0e32c94178f386d564286d603bb106c02
WolaDev <bozzbhoi@gmail.com>
2023-09-14 10:58:25 -0700

    101 print comb 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - possible combinations of two two-digits numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit1;
+	int digit2;
+
+	for (digit1 = 0; digit1 <= 9; digit1++)
+	{
+		for (digit2 = 0; digit2 <=9; digit2++)
+		{
+			putchar(digit1 + '0');
+			putchar(digit2 + '0');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
