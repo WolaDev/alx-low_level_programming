8be94925da801e7999eec287415782384ea303cf
WolaDev <bozzbhoi@gmail.com>
2023-09-14 23:05:23 -0700

    print comb of two two-digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,6 +9,8 @@ int main(void)
 {
 	int digit1;
 	int digit2;
+	int digit3;
+	int digit4;
 
 	for (digit1 = 0; digit1 <= 9; digit1++)
 	{
@@ -17,6 +19,17 @@ int main(void)
 			putchar(digit1 + '0');
 			putchar(digit2 + '0');
 			putchar(' ');
+	
+		for (digit3 = 0; digit3 <=9; digit3++)
+		{
+			for (digit4 = digit3 + 0; digit4 <= 9; digit4++)
+			{
+				putchar(digit3 + '0');
+				putchar(digit4 + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
 		}
 	}
 
