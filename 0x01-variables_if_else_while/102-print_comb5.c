45943f0a2632849bc3d84c96a0260eef29a37261
WolaDev <bozzbhoi@gmail.com>
2023-09-15 01:09:05 -0700

    Print two two-digits combination

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,9 +16,9 @@ int main(void)
 	{
 		for (digit2 = 0; digit2 <= 8; digit2++)
 		{
-			for (digit3 = digit2 +0; digit3 <= 9; digit3++)
+			for (digit3 = digit2 + 0; digit3 <= 9; digit3++)
 			{
-				for (digit4 = digit3 + 0; digit4 <= 9; digit4++)
+				for (digit4 = digit3 + 1; digit4 <= 9; digit4++)
 				{
 					putchar(digit1 + '0');
 					putchar(digit2 + '0');
