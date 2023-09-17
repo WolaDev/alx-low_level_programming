b5bfb550c9e56f1f109a72ec0cfafec06c3c5c33
WolaDev <bozzbhoi@gmail.com>
2023-09-17 14:38:57 -0700

    check is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check lowercase character
+ *
+ * Return: Always 0 (Success).
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
