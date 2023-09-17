807dbdee2ed681bc3bdd6699e5244707ebbd6896
WolaDev <bozzbhoi@gmail.com>
2023-09-16 23:23:51 -0700

    alphabet 10 times in lower case

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_10x - functionto print lowercase alphabet 10 times
+ * print_alphabet_x10 - functionto print lowercase alphabet 10 times
  *
  * Return: 0
  */
