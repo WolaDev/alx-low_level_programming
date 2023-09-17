defdc7bb83f2d4450b007f23583e411d160ef69e
WolaDev <bozzbhoi@gmail.com>
2023-09-16 23:06:06 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+
+		_putchar(alph);
+
+	_putchar('\n');
+}
