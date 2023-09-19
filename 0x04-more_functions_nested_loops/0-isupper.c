62cf33181eb5b71e428275bc3996fbad451e1d07
WolaDev <bozzbhoi@gmail.com>
2023-09-19 10:19:08 -0700

    check if it's uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check id a character is in uppercase
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c == 'A')
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+Read me file for the directory.
+
+This directory is for more functions and more nested loops
