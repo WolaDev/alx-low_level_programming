19eb8e32643720f9412c8664d45be006114cabe1
WolaDev <bozzbhoi@gmail.com>
2023-09-19 06:39:08 -0700

    add a and b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
