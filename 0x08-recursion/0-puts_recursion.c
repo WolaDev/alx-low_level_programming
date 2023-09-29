3b4ead3fa82683c1856e7d23a2d773ef6cd02bca
WolaDev <bozzbhoi@gmail.com>
2023-09-28 23:38:45 -0700

    0x08-recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - print a string followed by a new line
+ *
+ * Return: Always 0;
+ */
+int main(void)
+{
+	puts(_puts_recursion());
+	retun (0);
+}
