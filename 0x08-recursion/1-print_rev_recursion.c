d9f9c951b649a93e5739b56f864f469b28da5de4
WolaDev <bozzbhoi@gmail.com>
2023-09-30 08:59:21 -0700

    task 1 of 0x08-recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion: print out string in reverse recursion
+ * @s: second byte of the memory
+ * Return: -
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(*s + 1);
+	_putchar(*s);
+}
