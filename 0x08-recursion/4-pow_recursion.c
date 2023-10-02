4d18160e0d61ebd7e97fd60ae0a974eb0ec02f00
WolaDev <bozzbhoi@gmail.com>
2023-10-02 03:50:06 -0700

    x raise to power y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _pow_recursion - x raised to power y
+ * @x: parameter for whole number
+ * @y: parameter for power
+ * Return: -
+ */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
