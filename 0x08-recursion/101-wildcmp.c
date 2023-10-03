24cac5041ca575aa55aad9f60e55d0dd3a46e786
WolaDev <bozzbhoi@gmail.com>
2023-10-02 21:16:18 -0700

    0x08-recursion advance task 101

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * wildcmp - comparing two strings if they are thesame
+ * @s1: first bytes of the memory
+ * @s2: first bytes of the memory
+ *
+ * Return: -
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
+}
