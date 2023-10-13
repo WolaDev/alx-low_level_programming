a941fcfb6a0f88bab1095b366c83f9b6d7a36330
WolaDev <bozzbhoi@gmail.com>
2023-10-13 10:24:45 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print out all arguments
+ * @argc: parameter for argument count
+ * @argv: parameter for storing argv variables
+ *
+ * Description: -
+ * Return: -
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%s\n", argc);
+	return (0);
+}
