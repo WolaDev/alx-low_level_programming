92fbe1ec345cff74616d113a831f38569c4fbccb
WolaDev <bozzbhoi@gmail.com>
2023-10-12 23:58:59 -0700

    0-whatsmyname.c task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,16 @@
-#include "main.h"
+#include <stdio.h>
 
+/**
+ * main - entry point
+ * @argc: character
+ * @argv: character
+ *
+ * Description: -
+ * Return: -
+ */
 int main(int argc, char *argv[])
 {
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
 }
