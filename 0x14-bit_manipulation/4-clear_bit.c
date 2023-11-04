2126e17583968771271d4c5141c1f2ad714916f5
WolaDev <bozzbhoi@gmail.com>
2023-11-04 06:25:17 -0700

    Bit manipulation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,31 +1,16 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - check the code
- *
+ * clear_bit - function
+ * @n: a
+ * @index: a
  * Return: Always 0.
  */
-int main(void)
-{
-    unsigned long int n;
 
-    n = 1024;
-    set_bit(&n, 5);
-    printf("%lu\n", n);
-    n = 0;
-    set_bit(&n, 10);
-    printf("%lu\n", n);
-    n = 98;
-    set_bit(&n, 0);
-    printf("%lu\n", n);
-    return (0);
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
 }
-/*
-julien@ubuntu:~/0x14. Binary$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-set_bit.c -o d
-julien@ubuntu:~/0x14. Binary$ ./d
-1056
-1024
-99
-julien@ubuntu:~/0x14. Binary$
-*/
